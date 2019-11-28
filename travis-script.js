6a18e742c7b2ce56f3f574acc7e484705545500c
Preston Lim <prestonlimlianjie@gmail.com>
2019-11-28 13:27:07 +0800

    Delete travis-script.js

--- travis-script.js
+++
@@ -1,8 +0,0 @@
-//travis-script.js
-const travisScript = require("@isomerpages/isomerpages-travisci-scripts");
-
-const travisBranch = process.env.TRAVIS_BRANCH;
-
-if(travisBranch == "master") {
-  travisScript.purgeCacheOnly();
-}
